326abc3eee069848b0686fb2d68f88a0ef922cbc
Youngmin Kim <ymkim@huvitz.com>
2012-12-06 21:32:05 +0900

    dynamic language change

---
+++ lang/lang.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-05-03T15:32:39
+#
+#-------------------------------------------------
+
+TARGET = lang
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+TRANSLATIONS = lang_kr.ts \
+    lang_fr.ts
+
+RESOURCES += \
+    global.qrc
---
+++ lang/lang_kr.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ko_KR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translatorcomment>이게 어디서 사용되지?</translatorcomment>
+        <translation>메인윈도우</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>Hello</source>
+        <translation>안뇽</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="61"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>Korean</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="71"/>
+        <source>French</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="10"/>
+        <source>Hello World !!!</source>
+        <translation>안녕 세계 !!!</translation>
+    </message>
+</context>
+</TS>
