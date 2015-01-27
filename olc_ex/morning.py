94342d42ae48e9d3abed8df16b4f423480beb7a2
Youngmin Kim <ymkim@huvitz.com>
2015-01-27 13:19:44 +0900

    if

--- olc_ex/morning.py
+++ olc_ex/morning.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 morning_msg = u'좋은아침'
+goodday_msg = u'good afternoon'
+evening_msg = u'evening'
+have_coffee = u'coffee'
 
-hour = 11
+hour = 5
 
 if hour < 12:
     print(morning_msg)
+    print(have_coffee)
+elif hour < 18:
+    print(goodday_msg)
+else:
+    print(evening_msg)
+
