ade9771a620889a8e794b31d74427d202cf6e35f
Youngmin Kim <ymkim@huvitz.com>
2015-01-27 18:38:33 +0900

    list

---
+++ olc_ex/list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def maxNumber(x, y, z):
+    ''' 
+    get max number
+    '''
+
+    bigger = x;
+
+    if y > bigger:
+        bigger = y
+
+    if z > bigger:
+        bigger = z
+
+    return bigger
+
+
+
+    
+    
+def maxNumberList(numlist):
+    ''' '''
+
+    if type(numlist) != list :
+        printf("param is not list")
+        return
+
+    bigger = numlist[0]
+
+    for val in numlist:
+        if val > bigger:
+            bigger = val
+
+
+    return bigger
+
