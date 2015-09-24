ce07ae8b181def7cc0477d7d2cbe1b6e05f8cc97
Youngmin Kim <ymkim@huvitz.com>
2015-09-24 10:08:24 +0900

    script

--- float_perform/run.sh
+++ float_perform/run.sh
@@ -5,10 +5,10 @@ then
 	exit 0
 fi
 
-echo "time $1" >> out.txt
+echo "time $1" >> $2
 for i in 1 2 3 4 5 6 7 8 9 10
 do
-	/usr/bin/time -a -o out.txt -f "user %U system %S elapsed %e" $1 
+	/usr/bin/time -a -o $2 -f "user %U system %S elapsed %e" $1 
 done
 
-echo >> out.txt
+echo >> $2
