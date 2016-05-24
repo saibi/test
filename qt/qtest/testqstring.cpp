73e57acccfe989a99a3df1ee1e793a9fdb7e70e9
Youngmin Kim <ymkim@huvitz.com>
2016-05-24 17:14:46 +0900

    qtest

--- qt/qtest/testqstring.cpp
+++ qt/qtest/testqstring.cpp
@@ -42,13 +42,29 @@
 
 class TestQString:public QObject
 {
-	Q_OBJECT private slots:void toUpper();
+	Q_OBJECT 
+
+private slots:
+	void toUpper_data();
+	void toUpper();
 };
+
+void TestQString::toUpper_data()
+{
+	QTest::addColumn<QString>("string");
+	QTest::addColumn<QString>("result");
+
+	QTest::newRow("all lower") << "hello" << "HELLO";
+	QTest::newRow("mixed") << "Hello" << "HELLO";
+	QTest::newRow("all upper") << "HELLO" << "HELLO";
+}
 
 void TestQString::toUpper()
 {
-//	QVERIFY( QString("Hello").toUpper() == "HELLO W");
-	QCOMPARE(QString("Hello").toUpper(), QString("HELLO"));
+	QFETCH(QString, string);
+	QFETCH(QString, result);
+
+	QCOMPARE(string.toUpper(), result);
 }
 
 QTEST_MAIN(TestQString)
